1637ac5e571a503a796d253c5baeda1c610726fa
Nick Garcia <nick.agarcia103@gmail.com>
2015-06-18 22:52:44 -0500

    L & R Signs Website

---
+++ header.php
@@ -0,0 +1,103 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>L & R Signs</title>
+		<!--meta-->
+		<meta charset="UTF-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1.2" />
+		<meta name="format-detection" content="telephone=no" />
+		<meta name="keywords" content="Construction, Renovation" />
+		<meta name="description" content="Responsive Construction Renovation Template" />
+		<!--slider revolution-->
+		<link rel="stylesheet" type="text/css" href="rs-plugin/css/settings.css" media="screen" />
+		<!--style-->
+		<link href='//fonts.googleapis.com/css?family=Raleway:100,300,400,500,600,700,900' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" type="text/css" href="style/reset.css">
+		<link rel="stylesheet" type="text/css" href="style/superfish.css">
+		<link rel="stylesheet" type="text/css" href="style/prettyPhoto.css">
+		<link rel="stylesheet" type="text/css" href="style/jquery.qtip.css">
+		<link rel="stylesheet" type="text/css" href="style/style.css">
+		<link rel="stylesheet" type="text/css" href="style/animations.css">
+		<link rel="stylesheet" type="text/css" href="style/responsive.css">
+		<link rel="stylesheet" type="text/css" href="style/odometer-theme-default.css">
+		<!--fonts-->
+		<link rel="stylesheet" type="text/css" href="fonts/streamline-small/styles.css">
+		<link rel="stylesheet" type="text/css" href="fonts/streamline-large/styles.css">
+		<link rel="stylesheet" type="text/css" href="fonts/template/styles.css">
+		<link rel="stylesheet" type="text/css" href="fonts/social/styles.css">
+		<link rel="shortcut icon" href="images/favicon.ico">
+	</head>
+	<body>
+		<div class="site-container">
+			<div class="header-top-bar-container clearfix">
+				<div class="header-top-bar">
+					<ul class="contact-details clearfix">
+						<li class="template-phone">
+							(512) 412-2536
+						</li>
+						<li class="template-mail">
+							landrsigns@yahoo.com
+						</li>
+						<li class="template-clock">
+							Mon - Sun: 8:00am - 6:00pm
+						</li>
+					</ul>
+					<div class="search-container">
+						<a class="template-search" href="#" title="Search"></a>
+						<form class="search">
+							<input type="text" name="s" placeholder="Search..." value="Search..." class="search-input hint">
+							<fieldset class="search-submit-container">
+								<span class="template-search"></span>
+								<input type="submit" class="search-submit" value="">
+							</fieldset>
+							<input type="hidden" name="page" value="search">
+						</form>
+					</div>
+					<ul class="social-icons">
+						<li>
+							<a target="_blank" href="https://www.facebook.com/landrsigns" class="social-facebook" title="facebook"></a>
+						</li>
+						<!--<li>
+							<a target="_blank" href="https://twitter.com/QuanticaLabs" class="social-twitter" title="twitter"></a>
+						</li>
+						<li>
+							<a href="https://pinterest.com/quanticalabs/" class="social-pinterest" title="pinterest"></a>
+						</li>
+						<li>
+							<a title="" href="http://themeforest.net/user/QuanticaLabs?ref=QuanticaLabs" class="social-youtube"></a>
+						</li>
+						<li>
+							<a title="" href="http://themeforest.net/user/QuanticaLabs?ref=QuanticaLabs" class="social-linkedin"></a>
+						</li>
+						<li>
+							<a title="" href="http://themeforest.net/user/QuanticaLabs?ref=QuanticaLabs" class="social-pinterest"></a>
+						</li>
+						<li>
+							<a title="" href="http://themeforest.net/user/QuanticaLabs?ref=QuanticaLabs" class="social-instagram"></a>
+						</li>
+						
+						-->
+					</ul>
+				</div>
+				<a href="#" class="header-toggle template-arrow-up"></a>
+			</div>
+			<div class="header-container">
+			<!--<div class="header-container sticky">-->
+				<div class="vertical-align-table column-1-1">
+					<div class="header clearfix">
+						<div class="logo vertical-align-cell">
+							<a href="?page=home" title="L & R Signs"><img style="float: left;" src="images/logo.png" alt="L & R Signs" /></a>
+						</div>
+						<a href="#" class="mobile-menu-switch vertical-align-cell">
+							<span class="line"></span>
+							<span class="line"></span>
+							<span class="line"></span>
+						</a>
+						<div class="menu-container clearfix vertical-align-cell">
+							<?php
+							require_once('menu.php');
+							?>
+						</div>
+					</div>
+				</div>
+			</div>
---
+++ menu.php
@@ -0,0 +1,196 @@
+<nav>
+	<ul class="sf-menu">
+		<li<?php echo ($_GET["page"]=="" || $_GET["page"]=="home" ? " class='selected'" : ""); ?>>
+			<a href="?page=home" title="Home">
+				HOME
+			</a>
+		</li>
+		<li <?php echo ($_GET["page"]=="services" || $_GET["page"]=="service_interior_renovation" || $_GET["page"]=="service_design_build" || $_GET["page"]=="service_tiling_painting" || $_GET["page"]=="service_paver_walkways" || $_GET["page"]=="service_household_repairs" || $_GET["page"]=="service_solar_systems" ? " class='selected'" : ""); ?>>
+			<a href="?page=services" title="Services">
+				SERVICES
+			</a>
+			<ul>
+				<li<?php echo ($_GET["page"]=="service_interior_renovation" ? " class='selected'" : ""); ?>>
+					<a href="?page=service_interior_renovation" title="Interior Renovation">
+						Interior Renovation
+					</a>
+				</li>
+				<li<?php echo ($_GET["page"]=="service_design_build" ? " class='selected'" : ""); ?>>
+					<a href="?page=service_design_build" title="Design and Build">
+						Design and Build
+					</a>
+				</li>
+				<li<?php echo ($_GET["page"]=="service_tiling_painting" ? " class='selected'" : ""); ?>>
+					<a href="?page=service_tiling_painting" title="Design and Build">
+						Tiling and Painting
+					</a>
+				</li>
+				<!--<li<?php echo ($_GET["page"]=="service_paver_walkways" ? " class='selected'" : ""); ?>>
+					<a href="?page=service_paver_walkways" title="Paver Walkways">
+						Paver Walkways
+					</a>
+				</li>
+				<li<?php echo ($_GET["page"]=="service_household_repairs" ? " class='selected'" : ""); ?>>
+					<a href="?page=service_household_repairs" title="Household Repairs">
+						Household Repairs
+					</a>
+				</li>
+				<li<?php echo ($_GET["page"]=="service_solar_systems" ? " class='selected'" : ""); ?>>
+					<a href="?page=service_solar_systems" title="Solar Systems">
+						Solar Systems
+					</a>
+				</li>-->
+			</ul>
+		</li>
+		<!--<li <?php echo ($_GET["page"]=="projects" || $_GET["page"]=="project_interior_renovation" || $_GET["page"]=="project_garden_renovation" || $_GET["page"]=="project_painting" || $_GET["page"]=="project_design_build" || $_GET["page"]=="project_solar_systems" ? " class='selected'" : ""); ?>>
+			<a href="?page=projects" title="Projects">
+				PROJECTS
+			</a>
+			<ul>
+				<li<?php echo ($_GET["page"]=="project_interior_renovation" ? " class='selected'" : ""); ?>>
+					<a href="?page=project_interior_renovation" title="Interior Renovation">
+						Interior Renovation
+					</a>
+				</li>
+				<li<?php echo ($_GET["page"]=="project_garden_renovation" ? " class='selected'" : ""); ?>>
+					<a href="?page=project_garden_renovation" title="Garden Renovation">
+						Garden Renovation
+					</a>
+				</li>
+				<li<?php echo ($_GET["page"]=="project_painting" ? " class='selected'" : ""); ?>>
+					<a href="?page=project_painting" title="Painting">
+						Painting
+					</a>
+				</li>
+				<li<?php echo ($_GET["page"]=="project_design_build" ? " class='selected'" : ""); ?>>
+					<a href="?page=project_design_build" title="Design and Build">
+						Design and Build
+					</a>
+				</li>
+				<li<?php echo ($_GET["page"]=="project_solar_systems" ? " class='selected'" : ""); ?>>
+					<a href="?page=project_solar_systems" title="Solar Systems">
+						Solar Systems
+					</a>
+				</li>
+			</ul>
+		</li>-->
+		<li class="left-flyout<?php echo ($_GET["page"]=="contact" || $_GET["page"]=="contact_2" ? " selected" : ""); ?>">
+			<a href="?page=contact" title="Contact">
+				CONTACT
+			</a>
+		</li>
+	</ul>
+</nav>
+<div class="mobile-menu-container">
+	<div class="mobile-menu-divider"></div>
+	<nav>
+		<ul class="mobile-menu">
+			<li<?php echo ($_GET["page"]=="" || $_GET["page"]=="home" ? " class='selected'" : ""); ?>>
+				<a href="?page=home" title="Home">
+					HOME
+				</a>
+			</li>
+			<li <?php echo ($_GET["page"]=="services" || $_GET["page"]=="service_interior_renovation" || $_GET["page"]=="service_design_build" || $_GET["page"]=="service_tiling_painting" || $_GET["page"]=="service_paver_walkways" || $_GET["page"]=="service_household_repairs" || $_GET["page"]=="service_solar_systems" ? " class='selected'" : ""); ?>>
+				<a href="?page=services" title="Services">
+					SERVICES
+				</a>
+				<ul>
+					<li<?php echo ($_GET["page"]=="service_interior_renovation" ? " class='selected'" : ""); ?>>
+						<a href="?page=service_interior_renovation" title="Interior Renovation">
+							Interior Renovation
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="service_design_build" ? " class='selected'" : ""); ?>>
+						<a href="?page=service_design_build" title="Design and Build">
+							Design and Build
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="service_tiling_painting" ? " class='selected'" : ""); ?>>
+						<a href="?page=service_tiling_painting" title="Design and Build">
+							Tiling and Painting
+						</a>
+					</li>
+					<!--<li<?php echo ($_GET["page"]=="service_paver_walkways" ? " class='selected'" : ""); ?>>
+						<a href="?page=service_paver_walkways" title="Paver Walkways">
+							Paver Walkways
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="service_household_repairs" ? " class='selected'" : ""); ?>>
+						<a href="?page=service_household_repairs" title="Household Repairs">
+							Household Repairs
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="service_solar_systems" ? " class='selected'" : ""); ?>>
+						<a href="?page=service_solar_systems" title="Solar Systems">
+							Solar Systems
+						</a>
+					</li>-->
+				</ul>
+			</li>
+			<!--<li <?php echo ($_GET["page"]=="projects" || $_GET["page"]=="project_interior_renovation" || $_GET["page"]=="project_garden_renovation" || $_GET["page"]=="project_painting" || $_GET["page"]=="project_design_build" || $_GET["page"]=="project_solar_systems" ? " class='selected'" : ""); ?>>
+				<a href="?page=projects" title="Projects">
+					PROJECTS
+				</a>
+				<ul>
+					<li<?php echo ($_GET["page"]=="project_interior_renovation" ? " class='selected'" : ""); ?>>
+						<a href="?page=project_interior_renovation" title="Interior Renovation">
+							Interior Renovation
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="project_garden_renovation" ? " class='selected'" : ""); ?>>
+						<a href="?page=project_garden_renovation" title="Garden Renovation">
+							Garden Renovation
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="project_painting" ? " class='selected'" : ""); ?>>
+						<a href="?page=project_painting" title="Painting">
+							Painting
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="project_design_build" ? " class='selected'" : ""); ?>>
+						<a href="?page=project_design_build" title="Design and Build">
+							Design and Build
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="project_solar_systems" ? " class='selected'" : ""); ?>>
+						<a href="?page=project_solar_systems" title="Solar Systems">
+							Solar Systems
+						</a>
+					</li>
+				</ul>
+			</li>-->
+			<!--<li<?php echo ($_GET["page"]=="about" || $_GET["page"]=="404" ? " class='selected'" : ""); ?>>
+				<a href="?page=about" title="Pages">
+					PAGES
+				</a>
+				<ul>
+					<li<?php echo ($_GET["page"]=="about" ? " class='selected'" : ""); ?>>
+						<a href="?page=about" title="About">
+							About
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="404" ? " class='selected'" : ""); ?>>
+						<a href="?page=404" title="404 Not Found">
+							404 Not Found
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="service_interior_renovation" ? " class='selected'" : ""); ?>>
+						<a href="?page=service_interior_renovation" title="Single Service">
+							Single Service
+						</a>
+					</li>
+					<li<?php echo ($_GET["page"]=="project_interior_renovation" ? " class='selected'" : ""); ?>>
+						<a href="?page=project_interior_renovation" title="Single Project">
+							Single Project
+						</a>
+					</li>
+				</ul>
+			</li>-->
+			<li class="left-flyout<?php echo ($_GET["page"]=="contact" || $_GET["page"]=="contact_2" ? " selected" : ""); ?>">
+				<a href="?page=contact" title="Contact">
+					CONTACT
+				</a>
+			</li>
+		</ul>
+	</nav>
+</div>
