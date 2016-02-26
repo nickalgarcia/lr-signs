09f7c90715f8dc979445f65909e1ee83f32e349a
Blackbaud-NickGarcia <nick.garcia@blackbaud.com>
2016-02-25 21:14:21 -0600

    update to contact form settings

--- contact_form/config.php
+++ contact_form/config.php
@@ -1,13 +1,13 @@
 <?php
 define('_from_name', 'landrsigns.com form');
-define('_from_email', 'nick.agarcia@yahoo.com');
+define('_from_email', 'landrsigns@yahoo.com');
 
 define('_to_name', 'L and R Signs');
 define('_to_email', 'landrsigns@yahoo.com');
 
 define('_smtp_host', 'smtp.mail.yahoo.com');
 define('_smtp_username', 'nick.agarcia@yahoo.com');
-define('_smtp_password', 'H36r3w510_35');
+define('_smtp_password', 'J3r3m1@h29_13');
 define('_smtp_port', '465');
 define('_smtp_secure', 'ssl'); //ssl or tls
 
